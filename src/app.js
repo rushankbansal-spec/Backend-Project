52ccd3402245bdd5b1348262d2a0e80102541e37
Rushank Bansal <rushankbansal@gmail.com>
2026-06-24 18:37:38 +0530

    APi handelling

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.json({limit: "16kb"}));
+app.use(encodeURIComponent(express.urlencoded({ extended: true})));
+app.use(express.static("public"));
+export { app };
